d2279b7b75d1dff6f2f74231dd36ed2dcae46645
haiderseptem <muhammad.haider@septemsystems.com>
2025-09-17 17:39:38 +0500

    ecosystem updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "weather-backend",
-      script: "./src/index.js", // adjust if entry file is index.js/app.js
+      script: "./src/index.js",
       cwd: "./backend",
       instances: 1,
       autorestart: true,
@@ -10,17 +10,6 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         PORT: 5000
-      }
-    },
-    {
-      name: "weather-frontend",
-      script: "npx serve -s dist -l 3000", // using serve to host built frontend
-      cwd: "./frontend",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      env: {
-        NODE_ENV: "production"
       }
     }
   ]
